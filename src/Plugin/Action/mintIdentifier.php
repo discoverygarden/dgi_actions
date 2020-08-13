bb4e1f2985eaadbb3ec922be30c1ff8049cae529
Noel Chiasson <noel@discoverygarden.ca>
2020-08-13 18:22:56 -0300

    WIP: Minting and store in field

--- src/Plugin/Action/mintIdentifier.php
+++ src/Plugin/Action/mintIdentifier.php
@@ -19,24 +19,21 @@ class MintIdentifier extends IdentifierAction {
   /**
    * Constructs the data array into the expected metadata format for minting.
    */
-  protected function buildMetadata() {
-    // Pass in the data_profile config and entity reference.
-    // Separate the config key values into an array.
-    // Get the entity field values based on the key values.
-    // Construct and return the metadata string.
-    $testArray = [
-      'erc.who' => 'Test One',
-      'erc.what' => 'Test Two',
-      'erc.when' => '1900.01.20',
-    ];
+  protected function buildMetadata($data = null) {
+    if ($data) {
+      // Expects an associative array of key values. With keys and values matching the ERC data profile schema.
+      $outputString = "";
+      foreach($data as $key => $val) {
+        $outputString .= $key . ": " . $val . "\r\n";
+      }
 
-    // Expects an associative array of key values. With keys and values matching the ERC data profile schema.
-    $outputString = "";
-    foreach($testArray as $key => $val) {
-      $outputString .= $key . ": " . $val . "\r\n";
+      return $outputString;
     }
+    else {
+      $this->logger->warning('DGI_Actions - Build Metadata - Data is missing.');
 
-    return $outputString;
+      return "";
+    }
   }
 
   /**
@@ -47,52 +44,97 @@ class MintIdentifier extends IdentifierAction {
     $assocArray = [];
     foreach ($responseArray as $res_line) {
       $splitRes = explode(':', $res_line, 2);
-      $assocArray[$splitRes[0]] = $splitRes[1];
+      $assocArray[trim($splitRes[0])] = trim($splitRes[1]);
     }
 
     return $assocArray;
   }
 
+  public function getAssociatedConfigs() {
+    $configs = [];
+    $identifier = $this->configFactory->get($this->configuration['identifier_type']);
+    if (!empty($identifier->get())) {
+      $creds = $this->configFactory->get('dgi_actions.credentials.'.$identifier->get('identifier_id'));
+      $data_profile = $this->configFactory->get('dgi_actions.data_profile.'.$identifier->get('data_profile.id'));
+
+      $configs['identifier'] = $identifier;
+      $configs['credentials'] = $creds;
+      $configs['data_profile'] = $data_profile;
+
+      return $configs;
+    }
+
+    return FALSE;
+  }
+
+  protected function getFieldData($entity = null, $configs) {
+    // Test Entity Condition - Replace with Error.
+    if (!$entity) {
+      $entity = $this->entityTypeManager->getStorage('node')->load(3);
+    }
+
+    // Test Config Condition - Replace with Error.
+    if (!$configs) {
+      $configs = $this->getAssociatedConfigs();
+    }
+
+    $data = [];
+    foreach ($configs['data_profile']->get() as $key => $value) {
+      if(is_numeric($key)) {
+        $data[$value['key']] = $entity->get($value['source_field'])->getString(); //getvalue() and deal with array
+      }
+    }
+
+    $data = array_merge(['_target' => $entity->toUrl('canonical', ['absolute' => TRUE])->toString()], $data);
+
+    return $data;
+  }
+
+  protected function setIdentifierField($entity, $response) {
+    if (array_key_exists('success', $response)) {
+      $configs = $this->getAssociatedConfigs();
+      $entity->set($configs['credentials']->get('field'), $configs['credentials']->get('host').'/id/'.$response['success']);
+      $entity->save();
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
-  public function mint() {
-    // $this->configuration['identifier_type'] // Get the configured identifier for the action.
-    // Pull the configuration for the identifier.
-    // Pull the credentials and data profile for the identifier.
-    // Pass the data profile to the buildMetadata along with the entity.
-      // Method pulls applicable entity fields and constrcuts metadata.
-    // Send a client request based on pulled credentials to mint the identifier.
-    // Returns the mint request response.
-    $body = $this->buildMetadata();
+  public function mint($entity = null) {
+    $configs = $this->getAssociatedConfigs();
+    $body = $this->buildMetadata($this->getFieldData($entity, $configs));
 
-    try {
-      $response = $this->client->request('POST', $this->configuration['host'] . '/shoulder/' . $this->confinguration['namespace_shoulder'], [
-        'auth' => [$this->configuration['username'], $this->configuration['password']],
-        'headers' => [
-          'Content-Type' => 'text/plain; charset=UTF-8',
-          'Content-Length' => strlen($body)
-        ],
-        'body' => $body
-      ]);
-    }
-    catch (Exception $e) {
-      $this->logger->warning('Issue occurred while minting an identifier: @response', [
-        '@response' => $response,
-      ]);
-      return $e;
+    if (!empty($configs['credentials'])) {
+      try {
+        $response = $this->client->request('POST', $configs['credentials']->get('host') . '/shoulder/' . $configs['credentials']->get('shoulder'), [
+          'auth' => [$configs['credentials']->get('username'), $configs['credentials']->get('password')],
+          'headers' => [
+            'Content-Type' => 'text/plain; charset=UTF-8',
+            'Content-Length' => strlen($body)
+          ],
+          'body' => $body
+        ]);
+      }
+      catch (Exception $e) {
+        $this->logger->warning('DGI_Action - Mint Action - Issue occurred while minting an identifier: @response', [
+          '@response' => $response,
+        ]);
+        return $e;
+      }
+
+      return $this->responseArray($response->getBody()->getContents());
     }
 
-    return $this->responseArray($response->getBody()->getContents());
+    return $this->logger-warning('DGI_Action - Mint Action - Credentials Config not found.');
   }
 
   /**
    * {@inheritdoc}
    */
   public function execute($entity = null) {
-    // run $this->mint();
-    // Verify the returned response is success
-    // Write the minted ARK Identifier to the applicable field
+    //$response = $this->mint($entity);
+    //$this->setIdentifierField($entity, $response);
   }
 
   /**
@@ -124,6 +166,9 @@ class MintIdentifier extends IdentifierAction {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $entity = $this->entityTypeManager->getStorage('node')->load(3);
+
+    // Need a selection for content and bundle type
     $form['identifier_type'] = [
       '#type' => 'select',
       '#title' => t('Identifier Type'),
